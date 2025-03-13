13b13dea2993b2cd80b6ea6b70851f415e383a81
George Gannon <george.gannon2@bjss.com>
2025-03-13 17:34:48 +0000

    chapter 9 complete - all tests passed

--- rango/forms.py
+++ rango/forms.py
@@ -1,6 +1,7 @@
 from django import forms 
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
 from django.shortcuts import redirect, render 
+from django.contrib.auth.models import User
 
 
 class CategoryForm(forms.ModelForm):
@@ -29,3 +30,16 @@ class PageForm(forms.ModelForm):
         model = Page
         exclude = ('category',)
 
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')  # include password here
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile 
+        fields = ('website', 'picture',)
